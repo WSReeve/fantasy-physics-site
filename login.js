4870ed6505eaf5ff2417a3ae9049fe26d33339fa
ldeng688 <36999057+ldeng688@users.noreply.github.com>
2018-07-18 11:25:18 -0500

    Update login.js

--- login.js
+++ login.js
@@ -7,6 +7,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
     if(user != null){
       var email_id = user.email;
+      var email_verified = user.emailVerified;
       document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
       
   }
@@ -17,6 +18,19 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+function signup()
+{
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value; 
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+   
+   window.alert("Error: " + errorMessage);
+  // ...
+}
 
 function login(){
   

3f111775f42e580ed73fb10a30b39924cfebdf9e
ldeng688 <36999057+ldeng688@users.noreply.github.com>
2018-07-18 15:34:22 -0500

    Update login.js

--- login.js
+++ login.js
@@ -28,3 +28,14 @@ function login()
     });
     window.alert(errorMessage);
 }
+
+function logout()
+{
+    firebase.auth().signOut().then(function() 
+    {
+        // Sign-out successful.
+    }).catch(function(error) 
+    {
+        // An error happened.
+    });
+}

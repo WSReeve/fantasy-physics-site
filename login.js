5d78bfbec1a088a9bf93e48a02c48d006af97474
ldeng688 <36999057+ldeng688@users.noreply.github.com>
2018-07-18 15:17:26 -0500

    Update login.js

--- login.js
+++ login.js
@@ -5,13 +5,6 @@ firebase.auth().onAuthStateChanged(function(user)
       // User is signed in 
       document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";
-    
-      var user = firebase.auth().currentUser;
-    
-      if(user != null)
-      {
-          var email_id = user.email;
-          document.getElementId("user_para").innerHTML = "Welcome User: " + email_id;
   } 
   else 
   {

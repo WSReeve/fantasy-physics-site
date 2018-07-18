8673870f818ad1118763cf96ce5326ebcfba7c00
ldeng688 <36999057+ldeng688@users.noreply.github.com>
2018-07-18 12:27:01 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 firebase.auth().onAuthStateChanged(function(user) 
-{
+{ 
+  var user = firebase.auth().currentUser;
   if (user) 
   {
     // User is signed in.
     document.getElementById("user_div").style.display = "initial";
     document.getElementById("login_div").style.display = "none";
-    var user = firebase.auth().currentUser;
   }
   if(user != null)
   {
@@ -24,31 +24,20 @@ firebase.auth().onAuthStateChanged(function(user)
 
 function signup()
 {
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value; 
 
   firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
-   
-   window.alert("Error: " + errorMessage);
-  // ...
-})
+  
+});
 
 function login()
 {
-  
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  
- firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
-   
-   window.alert("Error: " + errorMessage);
-  // ...
 });
 }
 

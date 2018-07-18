c85f8132915adec057019d89f50aa6397e76f5d8
ldeng688 <36999057+ldeng688@users.noreply.github.com>
2018-07-18 11:18:44 -0500

    Update login.js

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
     if(user != null){
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
       
   }
   else {

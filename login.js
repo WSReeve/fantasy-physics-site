424a16b673795f2a0ab895a04bccc993d3f46280
ldeng688 <36999057+ldeng688@users.noreply.github.com>
2018-07-19 11:45:17 -0500

    Update login.js

--- login.js
+++ login.js
@@ -48,19 +48,6 @@ function signup()
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        if(errorCode == 'auth/email-already-in-use')
-        {
-            alert("This email already has an account. Please log in.");
-        }
-        else if(errorCode == 'auth/invalid-email')
-        {
-            alert("This email address is invalid. Try again.");
-        }
-        else
-        {
-            alert(errorMessage);
-        }
-        console.log(error);
     });
 }
 

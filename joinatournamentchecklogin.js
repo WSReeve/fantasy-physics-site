870df89252f98269c10412fcd8aeea8f74fba09d
Ayliose <41164814+Ayliose@users.noreply.github.com>
2018-08-09 11:17:59 -0500

    Update joinatournamentchecklogin.js

--- joinatournamentchecklogin.js
+++ joinatournamentchecklogin.js
@@ -1,8 +1,9 @@
 <script>
-var isLoggedIn = <?php print is_user_logged_in() ? 'true' : 'false'; ?>;
- if ( is_user_logged_in(true) )
-    $location = admin_url( 'joinatournament.html' );
-   if ( is_user_logged_in(false) )
-    $location = admin_url( 'login.html' );
-var redirectTo = '<?php print is_user_logged_in() ? admin_url( '/' ) : 'http://ldeng688.github.io/Fantasy-Physics/login.html'; ?>';
+var isLoggedIn = <?php print is_user_logged_in ? 'true' : 'false'; ?>;
+ if ( is_user_logged_in === true ) {
+   window.location.href = "joinatournament.html";
+}
+   if ( is_user_logged_in === false ) {
+    window.location.href = "login.html";
+}
 </script>
